563dcec516aa54204931a14dcc0f665e85d9d2b5
Stiven <stivengualli15@outlook.es>
2026-08-28 22:50:55 -0500

    matriz5x5

--- matriz5x5.py
+++ matriz5x5.py
@@ -1,7 +1,7 @@
 matriz = [[0 for _ in range(5)] for _ in range(5)]
 for i in range(5):
     for j in range(5):
-        valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
+        valor = int(input(f"Por favor ingrese un numero para la posición [{i}][{j}]: "))
         matriz[i][j] = valor
 print("\nMatriz ingresada:")
 for i in range(5):
